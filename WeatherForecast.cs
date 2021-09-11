f8737cc466024e37852ec8e120e863dd0532bda9
Leone Costa Rocha <leocr_cte@hotmail.com>
2021-09-11 17:32:05 -0300

    inicial

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CURSO_UDEMY_COGNIZANT_netcore31webapi
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
